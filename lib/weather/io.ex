faf39d14dcba73cddd21136efa0d4ece706d5523
Andy Park <andyp@opin.com>
2019-11-17 15:31:45 +0900

    Add documentation comments

--- lib/weather/io.ex
+++ lib/weather/io.ex
@@ -1,8 +1,15 @@
 defmodule Weather.IO do
+  @moduledoc """
+  Read/write functions for XML data
+  """
   require Logger
 
   @output_directory "output"
   
+  @doc """
+  Makes an output directory if it doesn't already exist and save contents to XML.
+  Returns <contents> to be piped to other functions
+  """
   def xml_to_file(name, contents) do
     { :ok, current } = File.cwd()
     dest = "#{current}/#{@output_directory}"
@@ -14,6 +21,9 @@ defmodule Weather.IO do
     contents
   end
   
+  @doc """
+  Write contents to an XML file at <dest>/<name>.xml
+  """
   defp save_xml(name, contents, dest) do
     IO.puts "Name: #{name}"
     :ok = File.write("#{dest}/#{name}.xml", contents)
